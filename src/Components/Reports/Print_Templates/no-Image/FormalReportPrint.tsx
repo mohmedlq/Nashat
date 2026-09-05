1fd2c28db8b30c7614a2e92d2f0060b99d17f54c
Mohamed <mohmedisa630@gmail.com>
2026-09-05 22:29:57 +0300

    #

--- src/Components/Reports/Print_Templates/no-Image/FormalReportPrint.tsx
+++ src/Components/Reports/Print_Templates/no-Image/FormalReportPrint.tsx
@@ -563,6 +563,7 @@ export const FormalReportPrint = React.forwardRef<
                   focus:!border-0
                   focus:!outline-none
                   focus:!ring-0
+                  h-full
                 "
               />
             </div>
