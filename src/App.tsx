4e5309f6de9bec7dd054199b87da7d025f5ab09d
Mohamed <mohmedisa630@gmail.com>
2026-09-11 22:48:44 +0300

    Speed Insights Removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import AppRoutes from './Routes/Routes';
-import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Analytics } from "@vercel/analytics/react"
 function App() {
   return <AppRoutes />;
